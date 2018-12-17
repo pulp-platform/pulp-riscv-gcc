641175fa6b3045e7f42e62831925457c40f9a895
Alessandro Capotondi <alessandro.capotondi@unibo.it>
2018-12-17 16:00:35 +0100

    WIP: MRWOLFE TOOLCHAIN
    WIP. Adding heterogenous offloading for mrwolfe chip.

---
+++ gcc/config/riscv/mrwolfe-offload.h
@@ -0,0 +1,34 @@
+/*
+ * Offload image generation tool for RISC-V PULP HERO device.
+ *
+ * Copyright (C) 2018 ETH Zurich and University of Bologna
+ * 
+ * Author: Alessandro Capotondi, UNIBO, (alessandro.capotondi@unibo.it)
+ * 
+ * This file is part of GCC.
+ * 
+ * GCC is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ * 
+ * GCC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with GCC; see the file COPYING3.  If not see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef HERO_OFFLOAD_H
+#define HERO_OFFLOAD_H
+
+/* Support for OpenACC acc_on_device.  */
+#include "gomp-constants.h"
+
+#define ACCEL_COMPILER_acc_device GOMP_DEVICE_PULP_MRWOLFE
+
+#endif
+
